cbc937ec87482c419254e3b9fc4bc14befe3569e
Sahil Lalani <69220306+Sahillalani070@users.noreply.github.com>
2025-08-14 14:47:33 -0400

    WIP: Auth changes

---
+++ app/firebase/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDnxzDcEWfiexlq2wMQyd_7vqIQhv8_8No",
+    authDomain: "focusflowstore-235ff.firebaseapp.com",
+    projectId: "focusflowstore-235ff",
+    storageBucket: "focusflowstore-235ff.firebasestorage.app",
+    messagingSenderId: "497380832027",
+    appId: "1:497380832027:web:da944504e3609cfa4a59be",
+    measurementId: "G-20DMT87524"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Init services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ app/register.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, Text } from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
+import { useRouter } from "./(tabs)/";
+
+export default function LoginScreen() {
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            router.replace("")
+        }
+    }
+}
